2075ea21ca6968cd318abe058d88e0d8a753d9ef
xlauko <xlauko@mail.muni.cz>
2016-12-12 10:36:35 +0100

    try threshold

--- pca/filter.hpp
+++ pca/filter.hpp
@@ -6,23 +6,26 @@ using Image = cv::Mat;
 
 namespace {
     Image get_map ( Image & img ) {
-        Image res;
-        cv::threshold( cv::Scalar::all( 255 ) - img, res, 214, 255, cv::THRESH_BINARY );
-        cv::blur( res, res, cv::Size( 3, 3 ) );
-        cv::Canny( res, res, 100, 200, 3);
+        Image res = img;
+        //cv::threshold( img, res, 214, 255, cv::THRESH_BINARY );
+        //cv::threshold( cv::Scalar::all( 255 ) - img, res, 214, 255, cv::THRESH_BINARY );
+        //cv::blur( res, res, cv::Size( 3, 3 ) );
+        /*cv::Canny( res, res, 100, 200, 3);
 
         cv::Mat element = cv::getStructuringElement( cv::MORPH_ELLIPSE, cv::Size( 5, 5 ) );
 
-        cv::morphologyEx( res, res, cv::MORPH_CLOSE, element );
+        cv::morphologyEx( res, res, cv::MORPH_CLOSE, element );*/
         return res;
     }
 }
 
 static Image filter ( Image & img ) {
 	Image res;
-    auto map = get_map( img );
-	cv::Canny( img, res, 40, 200, 3 );
-	return res - map;
+    //auto map = get_map( img );
+	//cv::Canny( img, res, 40, 200, 3 );
+	//return res - map;
+    cv::threshold( img, res, 200, 255, cv::THRESH_BINARY );
+    return res;
 }
 
 } /* namespace filter */
